abefa3a316e531e3a4e99c954cfe2f518358ce65
JackTh3 <166152697+JackTh3@users.noreply.github.com>
2024-05-28 16:48:00 -0400

    more config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build' // Specify the output directory for the built files
+  },
   base: "/Weather360/"
 })
